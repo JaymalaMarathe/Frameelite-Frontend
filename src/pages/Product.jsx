6289ddb77ef9264768631b7b2ed5bf467125426e
Your Name <you@example.com>
2022-07-14 23:28:32 +0530

    Changed UI

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,173 +1,87 @@
-import { Add, Remove } from "@material-ui/icons";
+import {
+  FavoriteBorderOutlined,
+  SearchOutlined,
+  ShoppingCartOutlined,
+} from "@material-ui/icons";
+import { Tooltip } from "@mui/material";
 import styled from "styled-components";
-import Announcement from "../components/Announcement";
-import Footer from "../components/Footer";
-import Navbar from "../components/Navbar";
-import Newsletter from "../components/Newsletter";
-import { mobile } from "../responsive";
 
-const Container = styled.div``;
-
-const Wrapper = styled.div`
-  padding: 50px;
-  display: flex;
-  ${mobile({ padding: "10px", flexDirection:"column" })}
-`;
-
-const ImgContainer = styled.div`
-  flex: 1;
-`;
-
-const Image = styled.img`
+const Info = styled.div`
+  opacity: 0;
   width: 100%;
-  height: 90vh;
-  object-fit: cover;
-  ${mobile({ height: "40vh" })}
-`;
-
-const InfoContainer = styled.div`
-  flex: 1;
-  padding: 0px 50px;
-  ${mobile({ padding: "10px" })}
-`;
-
-const Title = styled.h1`
-  font-weight: 200;
-`;
-
-const Desc = styled.p`
-  margin: 20px 0px;
-`;
-
-const Price = styled.span`
-  font-weight: 100;
-  font-size: 40px;
-`;
-
-const FilterContainer = styled.div`
-  width: 50%;
-  margin: 30px 0px;
-  display: flex;
-  justify-content: space-between;
-  ${mobile({ width: "100%" })}
-`;
-
-const Filter = styled.div`
-  display: flex;
-  align-items: center;
-`;
-
-const FilterTitle = styled.span`
-  font-size: 20px;
-  font-weight: 200;
-`;
-
-const FilterColor = styled.div`
-  width: 20px;
-  height: 20px;
-  border-radius: 50%;
-  background-color: ${(props) => props.color};
-  margin: 0px 5px;
-  cursor: pointer;
-`;
-
-const FilterSize = styled.select`
-  margin-left: 5px;
-  padding: 15px;
-  
-  font-weight: 600;
-`;
-
-const FilterSizeOption = styled.option``;
-
-const AddContainer = styled.div`
-  width: 50%;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  ${mobile({ width: "100%" })}
-`;
-
-const AmountContainer = styled.div`
-  display: flex;
-  align-items: center;
-  font-weight: 700;
-`;
-
-const Amount = styled.span`
-  width: 30px;
-  height: 30px;
-  border-radius: 10px;
-  border: 1px solid teal;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  background-color: rgba(0, 0, 0, 0.2);
+  z-index: 3;
   display: flex;
   align-items: center;
   justify-content: center;
-  margin: 0px 5px;
+  transition: all 0.5s ease;
+  cursor: pointer;
 `;
 
-const Button = styled.button`
-  padding: 15px;
-  border: 2px solid teal;
-  background-color: white;
-  cursor: pointer;
-  font-weight: 700;
+const Container = styled.div`
+  flex: 1;
+  margin: 5px;
+  min-width: 280px;
+  height: 350px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #f5fbfd;
+  position: relative;
 
-  &:hover{
-      background-color: #f8f4f4;
+  &:hover ${Info}{
+    opacity: 1;
   }
 `;
 
-const Product = () => {
+const Circle = styled.div`
+  width: 200px;
+  height: 200px;
+  border-radius: 50%;
+  background-color: white;
+  position: absolute;
+`;
+
+const Image = styled.img`
+  height: 75%;
+  z-index: 2;
+`;
+
+const Icon = styled.div`
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  background-color: white;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 10px;
+  transition: all 0.5s ease;
+  &:hover {
+    background-color: #e9f5f5;
+    transform: scale(1.1);
+  }
+`;
+
+const Product = ({ item }) => {
   return (
     <Container>
-      <Navbar />
-      <Announcement />
-      <Wrapper>
-        <ImgContainer>
-          <Image src="https://m.media-amazon.com/images/I/71Of9j9iM6L._SL1100_.jpg" />
-        </ImgContainer>
-        <InfoContainer>
-          <Title> Multicolour Framed Poster of Butterfly (14x14inch, Synthetic Wood)</Title>
-          <Desc>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-            venenatis, dolor in finibus malesuada, lectus ipsum porta nunc, at
-            iaculis arcu nisi sed mauris. Nulla fermentum vestibulum ex, eget
-            tristique tortor pretium ut. Curabitur elit justo, consequat id
-            condimentum ac, volutpat ornare.
-          </Desc>
-          <Price>₹ 500</Price>
-          <FilterContainer>
-            <Filter>
-              <FilterTitle>Frame Color</FilterTitle>
-              <FilterColor color="black" />
-              <FilterColor color="darkblue" />
-              <FilterColor color="gray" />
-              <FilterColor color="brown" />
-              <FilterColor color="white" />
-            </Filter>
-            <Filter>
-              <FilterTitle>Frame Size</FilterTitle>
-              <FilterSize>
-                <FilterSizeOption>20X40 (Inch)</FilterSizeOption>
-                <FilterSizeOption>15X20 (Inch)</FilterSizeOption>
-                <FilterSizeOption>10X10 (Inch)</FilterSizeOption>
-                <FilterSizeOption>8X12 (Inch)</FilterSizeOption>
-                <FilterSizeOption>5X10 (Inch)</FilterSizeOption>
-              </FilterSize>
-            </Filter>
-          </FilterContainer>
-          <AddContainer>
-            <AmountContainer>
-              <Remove />
-              <Amount>1</Amount>
-              <Add />
-            </AmountContainer>
-            <Button>ADD TO CART</Button>
-          </AddContainer>
-        </InfoContainer>
-      </Wrapper>
-      <Newsletter />
-      <Footer />
+      <Circle />
+      <Image src={item.img} />
+      <Info>
+        <Icon>
+        <Tooltip title="Add to Cart"><ShoppingCartOutlined /></Tooltip>
+        </Icon>
+        <Icon><Tooltip title="Search"><SearchOutlined /></Tooltip>
+           </Icon>
+        <Icon><Tooltip title="Add to Wishlist"><FavoriteBorderOutlined /></Tooltip>
+          
+        </Icon>
+      </Info>
     </Container>
   );
 };
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,5 +1,7 @@
 import styled from "styled-components";
 import { mobile } from "../responsive";
+import {Button,Link} from '@mui/material';
+// import PersonOutlineIcon from '@mui/icons-material';
 
 const Container = styled.div`
   width: 100vw;
@@ -9,7 +11,6 @@ const Container = styled.div`
       rgba(255, 255, 255, 0.5)
     ),
     url("https://img.freepik.com/free-vector/abstract-background_53876-43362.jpg?w=2000")
-      center;
   background-size: cover;
   display: flex;
   align-items: center;
@@ -19,6 +20,8 @@ const Container = styled.div`
 const Wrapper = styled.div`
   width: 40%;
   padding: 20px;
+  border-radius: 25px;
+  border: 2px ;
   background-color: white;
   ${mobile({ width: "75%" })}
 `;
@@ -30,7 +33,7 @@ const Title = styled.h1`
 
 const Form = styled.form`
   display: flex;
-  flex-wrap: wrap;
+  flex-direction: column;
 `;
 
 const Input = styled.input`
@@ -45,32 +48,26 @@ const Agreement = styled.span`
   margin: 20px 0px;
 `;
 
-const Button = styled.button`
-  width: 40%;
-  border: none;
-  padding: 15px 20px;
-  background-color: teal;
-  color: white;
-  cursor: pointer;
-`;
 
 const Register = () => {
   return (
     <Container>
       <Wrapper>
-        <Title>CREATE AN ACCOUNT</Title>
+        <Title><center>CREATE AN ACCOUNT</center></Title>
+        <br/>
+        <center>Already Have an Account? <Link>Sign In</Link></center>
         <Form>
-          <Input placeholder="name" />
-          <Input placeholder="last name" />
-          <Input placeholder="username" />
-          <Input placeholder="email" />
-          <Input placeholder="password" />
-          <Input placeholder="confirm password" />
+          <Input placeholder="Name" />
+          <Input placeholder="Last Name" />
+          <Input placeholder="Username" />
+          <Input placeholder="Email" />
+          <Input placeholder="Password" />
+          <Input placeholder="Confirm Password" />
           <Agreement>
             By creating an account, I consent to the processing of my personal
             data in accordance with the <b>PRIVACY POLICY</b>
           </Agreement>
-          <Button>CREATE</Button>
+          <Button>CREATE ACCOUNT</Button>
         </Form>
       </Wrapper>
     </Container>
